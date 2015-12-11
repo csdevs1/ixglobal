7e18dc22cfd5b3efa9a93bb0abbf5abd5879b903
csdevs1 <csdevs1@gmail.com>
2015-12-11 14:39:03 -0430

    Improved design, added milestone

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,19 @@
 $(document).ready(function($) {
-    "use strict";
-    function count($this){
-        var current = parseInt($this.html(), 10);
-        current = current + 0; /* Where 50 is increment */	
-        $this.html(++current);
-        if(current > $this.data('count')){
-            $this.html($this.data('count'));
-        } else {    
-            setTimeout(function(){count($this)}, 100);
-        }
-    }        	
-    $(".stat-count").each(function() {
-        $(this).data('count', parseInt($(this).html(), 10));
-        $(this).html('0');
-        count($(this));
-    });
+    setTimeout(function() {
+        function count($this){
+            var current = parseInt($this.html(), 10);
+            current = current + 0; /* Where 50 is increment */	
+            $this.html(++current);
+            if(current > $this.data('count')){
+                $this.html($this.data('count'));
+            } else {    
+                setTimeout(function(){count($this)}, 150);
+            }
+        }        	
+        $(".stat-count").each(function() {
+            $(this).data('count', parseInt($(this).html(), 10));
+            $(this).html('0');
+            count($(this));
+        });
+    }, 200);
 });
