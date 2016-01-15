008329e6d45c1217a27e377db511a0c25eacdadf
csdevs1 <csdevs1@gmail.com>
2016-01-14 23:25:41 -0430

    changed logo, moved fonts to 'fonts folder'

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,9 @@
 $(document).ready(function($) {
-    setTimeout(function() {
+ /*   setTimeout(function() {
         function count($this){
             var current = parseInt($this.html(), 10);
             current = current + 0; /* Where 50 is increment */	
-            $this.html(++current);
+           /* $this.html(++current);
             if(current > $this.data('count')){
                 $this.html($this.data('count'));
             } else {    
@@ -15,9 +15,46 @@ $(document).ready(function($) {
             $(this).html('0');
             count($(this));
         });
-    }, 200);
+    }, 200);*/
 });
 /* Fade out on Scroll
 $(window).scroll(function() {
     $(".logo").css({'opacity': 1-(($(this).scrollTop())/250)});          
 });*/
+
+$(function () {
+    var fx = function fx() {
+    $(".stat-number").each(function (i, el) {
+        var data = parseInt(this.dataset.n, 10);
+        var props = {
+            "from": {
+                "count": 0
+            },
+                "to": {
+                "count": data
+            }
+        };
+        $(props.from).animate(props.to, {
+            duration: 1500 * 1,
+            step: function (now, fx) {
+                $(el).text(Math.ceil(now));
+            },
+            complete:function() {
+                if (el.dataset.sym !== undefined) {
+                  el.textContent = el.textContent.concat(el.dataset.sym)
+                }
+            }
+        });
+    });
+    };
+    
+    var reset = function reset() {
+        console.log($(this).scrollTop())
+        if ($(this).scrollTop() > 250) {
+            $(this).off("scroll");
+          fx()
+        }
+    };
+    
+    $(window).on("scroll", reset);
+});
